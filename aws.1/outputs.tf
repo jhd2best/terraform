e4316e95a796b7fbb010ac3f48a21215d60ef04e
jhd2best <82733821@qq.com>
2020-09-09 15:00:30 +0800

    [aws]change output script

--- aws.1/outputs.tf
+++ aws.1/outputs.tf
@@ -1,9 +1,9 @@
 output "public_ip" {
   description = "Public IP of instance (or EIP)"
-  value       = aws_spot_instance_request.foundation-node.*.public_ip
+  value       = aws_instance.foundation-node.*.public_ip
 }
 
 output "instance_id" {
   description = "Instance ID"
-  value       = aws_spot_instance_request.foundation-node.*.spot_instance_id
+  value       = aws_instance.foundation-node.*.id
 }
