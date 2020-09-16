a6366c44b42b8a7e1f398b52aacecfd99c2aa93e
jhd2best <82733821@qq.com>
2020-09-16 23:32:57 +0800

    change azure setup security group

--- azure.setup/main.tf
+++ azure.setup/main.tf
@@ -16,28 +16,18 @@ resource "azurerm_network_security_group" "main" {
   resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
-    name                       = "harmony"
+    name                       = "harmony_basic_port"
     priority                   = 110
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_ranges    = ["9000", "9100", "6000", "9500"]
+    destination_port_ranges    = ["9000", "6000", "14555"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
-  security_rule {
-    name                       = "manage"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  /* need add special security group rules */
 
   security_rule {
     name                       = "otbound-default"
